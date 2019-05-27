b952ad7fbea21f70402ba817d5ff89dbc22c6bfb
Lucas Marques <lucas.msilva09@gmail.com>
2019-05-26 23:31:40 -0300

    Creating specific view models

---
+++ src/blogger.api/ViewModels/EditorArticleViewModel.cs
@@ -0,0 +1,9 @@
+namespace blogger.api.ViewModels
+{
+    public class EditorArticleViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ src/blogger.api/ViewModels/ResultViewModel.cs
@@ -0,0 +1,9 @@
+namespace blogger.api.ViewModels
+{
+    public class ResultViewModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
