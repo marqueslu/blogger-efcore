2c77edf3a8645633e32cd82c18b4e541d6988fa9
Lucas Marques <lucas.msilva09@gmail.com>
2019-05-26 23:33:24 -0300

    Documenting the API

--- src/blogger.api/Models/Article.cs
+++ src/blogger.api/Models/Article.cs
@@ -4,7 +4,7 @@ namespace blogger.api.Models
 {
     public class Article
     {
-        public Article(int id, string title, string content, DateTime createdAt, DateTime updatedAt)
+        public Article(string title, string content, DateTime createdAt, DateTime updatedAt)
         {
             if(string.IsNullOrEmpty(title)){
                 throw new ArgumentException("Invalid title");
@@ -14,12 +14,11 @@ namespace blogger.api.Models
                 throw new ArgumentException("Invalid Content");
             }
             
-            Id = id;
             Title = title;
             Content = content;
             CreatedAt = createdAt;
             UpdatedAt = updatedAt;
-        }
+        }        
 
         public int Id { get; set; }
         public string Title { get; set; }
--- src/blogger.api/Startup.cs
+++ src/blogger.api/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using blogger.api.Data;
+using blogger.api.Repositories;
+using blogger.api.Repositories.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace blogger.api
 {
@@ -15,6 +19,12 @@ namespace blogger.api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddScoped<BloggerDataContext, BloggerDataContext>();
+            services.AddTransient<IArticlerepository, ArticleRepository>();
+            services.AddSwaggerGen(x => {
+                x.SwaggerDoc("v1", new Info { Title = "Blogger", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +36,11 @@ namespace blogger.api
 
 
                 app.UseMvc();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blogger B1");
+                });
             }
             
         }
